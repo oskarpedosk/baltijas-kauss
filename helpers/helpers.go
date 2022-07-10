d0921f577f7fb26d2ee69c8af8ef0d24df6ac43e
Oskar Pedosk <op@Oskars-Air.lan>
2022-07-10 23:59:59 +0300

    added helpers

---
+++ helpers/helpers.go
@@ -0,0 +1,17 @@
+package helpers
+
+import (
+	"encoding/json"
+	"io/ioutil"
+	"os"
+)
+
+func writeToJson(fileName string, sliceOfPlayers interface{}) {
+	// Create a file
+	file,_ := os.Create(fileName)
+	defer file.Close()
+
+	// Write scraped data to .json
+	data,_ := json.MarshalIndent(sliceOfPlayers, "", "	")	
+	ioutil.WriteFile(fileName, data, 0644)
+}
