ee0b1d20dae091bae89c34a05a460c33dba359b6
Oskar Pedosk <op@Oskars-Air.lan>
2022-07-10 18:50:43 +0300

    web scraping complete

--- main.go
+++ main.go
@@ -2,44 +2,28 @@ package main
 
 import (
 	"fmt"
+	"strconv"
 	"strings"
 
 	"github.com/gocolly/colly"
 )
 
 // NBA player
-type PlayerInfo struct {
-	PlayerID 			string 
-	PlayerName 			string
-	PlayerPositions 	[]string
-	PlayerTeam 			string
-}
-type Height struct {
-	Feet 	int
-	Inches 	int
-}
-type PlayerStats struct {
-	PlayerHeight 		Height
-	PlayerOverallRating int
-	Player3ptRating 	int
-	PlayerDunkRating 	int
-}
-//type Player struct {
-//	info PlayerInfo
-//	stats PlayerStats
-//}
-
-func trimSpace(str string) string {
-	str = strings.TrimSpace(str)
-	return str
-}
-
-func trim(str string, condition string) string {
-	str = strings.Trim(str, condition)
-	return str
+type PlayerData struct {
+	playerRank			int
+	playerName			string
+	playerPositions		[]string
+	playerTeam			string
+	playerHeight		[]int
+	playerRatings 		[]int
 }
 
 func main() {
+	scrapeDataFromURL("https://www.2kratings.com/lists/top-100-highest-nba-2k-ratings")
+	fmt.Println(PlayerData)
+}
+
+func scrapeDataFromURL(scrapeUrl string) {
 	// creating a file
 	/*
     fName := "data.csv"
@@ -53,49 +37,71 @@ func main() {
     writer := csv.NewWriter(file)
     defer writer.Flush()
 	*/
-	scrapeUrl := "https://www.2kratings.com/lists/top-100-highest-nba-2k-ratings"
 
 	c := colly.NewCollector()
     c.OnHTML("div.table-responsive tbody", func(e *colly.HTMLElement) {
+		i := 1
 		e.ForEach("tr", func(_ int, el *colly.HTMLElement) {
 			if el.Text != "" {
-				playerID := trim(el.ChildText("td.counter"), ".")
+				// Get player name
 				playerName := el.ChildText("td:nth-child(2) div.entries span.entry-font")
+				
+				// Get player info (positions, height, team)
 				playerInfo := strings.Split(el.ChildText("td:nth-child(2) div.entries span.entry-subtext-font"), "|")
+
+				// Get player positions from player info
+				playerPositions := strings.Split(playerInfo[0], "/")
+				for i := range playerPositions {
+					playerPositions[i] = trimSpace(playerPositions[i])
+				}
+
+				// Get player height from player info
 				playerHeightData := strings.Split(playerInfo[1], "'")
 				for i := range playerHeightData {
 					playerHeightData[i] = trimSpace(playerHeightData[i])
 				}
 				playerHeightFeet := playerHeightData[0]
 				playerHeightInches := trim(playerHeightData[1], "\"")
-				playerHeight := make([]string, 2)
-				playerHeight = append(playerHeight, playerHeightFeet, playerHeightInches)
+				playerHeight := []int{}
+				playerHeight = append(playerHeight, atoi(playerHeightFeet), atoi(playerHeightInches))
+
+				// Get player team from player info
 				playerTeam := trimSpace(playerInfo[2])
-				playerPosition := strings.Split(playerInfo[0], "/")
-				playerOverallRating := el.ChildText("td:nth-child(3)")
-				player3ptRating := el.ChildText("td:nth-child(4)")
-				playerDunkRating := el.ChildText("td:nth-child(5)")
-				for i := range playerPosition {
-					playerPosition[i] = trimSpace(playerPosition[i])
-				}
+
+				// Get player ratings
+				playerRatings := []int{}
+				playerOverallRating := atoi(el.ChildText("td:nth-child(3)"))
+				player3ptRating := atoi(el.ChildText("td:nth-child(4)"))
+				playerDunkRating := atoi(el.ChildText("td:nth-child(5)"))
+				playerRatings = append(playerRatings, playerOverallRating, player3ptRating, playerDunkRating)
 				
-				fmt.Println(playerID, playerName, playerHeight, playerPosition, playerOverallRating, player3ptRating, playerDunkRating, playerTeam)
+				// Add data to struct
+				data := PlayerData{}
+				data.playerRank = i
+				i++
+				data.playerName = playerName
+				data.playerPositions = playerPositions
+				data.playerTeam = playerTeam
+				data.playerHeight = playerHeight
+				data.playerRatings = playerRatings
 			}
-			
-			
-			/*
-			if playerID != "" {
-				writer.Write([]string {
-					playerID,
-					playerName,
-					playerOverallRating,
-					
-				})
-			}
-			*/
 		})
-        
         fmt.Println("Scrapping Complete")	
     })
     c.Visit(scrapeUrl)
+}
+
+func trimSpace(str string) string {
+	str = strings.TrimSpace(str)
+	return str
+}
+
+func trim(str string, condition string) string {
+	str = strings.Trim(str, condition)
+	return str
+}
+
+func atoi(str string) int {
+	integer,_ := strconv.Atoi(str)
+	return integer
 }
