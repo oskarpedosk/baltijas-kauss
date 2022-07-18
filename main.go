101cd75bc5c8741db278371bbfea23aa897a516d
Oskar Pedosk <op@Oskars-Air.lan>
2022-07-18 09:59:36 +0300

    reading json files

--- main.go
+++ main.go
@@ -7,4 +7,5 @@ import (
 func main() {
 	scrapedData := utilities.ScrapeDataFromURL("https://www.2kratings.com/lists/top-100-highest-nba-2k-ratings")
 	utilities.WriteToJson("player_data.json", scrapedData)
+	utilities.ReadJson("player_data.json")
 }
--- utilities/readjson.go
+++ utilities/readjson.go
@@ -1,7 +1,9 @@
 package utilities
 
 import (
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"os"
 )
 
@@ -14,5 +16,27 @@ func ReadJson(fileName string) {
 	fmt.Println("Successfully opened ", fileName)
 	// Defer the closing of our jsonFile so that we can parse it later on
 	defer jsonFile.Close()
+
+	// Read our opened jsonFile as a byte array.
+	byteValue, _ := ioutil.ReadAll(jsonFile)
+
+	// Initialize our players array
+	var players []PlayerData
+
+	// Unmarshal our byteArray which contains our
+	// jsonFile's content into 'players' which we defined above
+	json.Unmarshal(byteValue, &players)
+
+	// Iterate through every player within our players array
+	for i := 0; i < len(players); i++ {
+		fmt.Println("Rank: ", players[i].Rank)
+		fmt.Println(players[i].Name, players[i].Team)
+		fmt.Println("Positions: ", players[i].Positions)
+		fmt.Println("Height: ", players[i].Height)
+		fmt.Println("Overall: ", players[i].OverallRating)
+		fmt.Println("3pt: ", players[i].ThreePointRating)
+		fmt.Println("Dunk: ", players[i].DunkRating)
+		fmt.Println("--------------")
+	}
 
 }
