40de983d32d47cddf4edbece5aaa1971d1ca18cf
Oskar Pedosk <op@Oskars-Air.lan>
2022-07-19 10:46:18 +0300

    working readjson

--- utilities/readjson.go
+++ utilities/readjson.go
@@ -12,8 +12,10 @@ func ReadJson(fileName string) []PlayerData {
 	jsonFile, err := os.Open(fileName)
 	if err != nil {
 		fmt.Println(err)
+	} else {
+		fmt.Println("Successfully opened ", fileName)
 	}
-	fmt.Println("Successfully opened ", fileName)
+
 	// Defer the closing of our jsonFile so that we can parse it later on
 	defer jsonFile.Close()
 
