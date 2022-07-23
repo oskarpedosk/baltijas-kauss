fc9e9afaefb527d7e33ab34e0a5ab472919dfa28
Oskar Pedosk <op@Oskars-Air.lan>
2022-07-23 23:54:09 +0300

    added middleware and mux

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -24,14 +24,19 @@ func main() {
 
 	repo := handlers.NewRepo(&app)
 	handlers.NewHandlers(repo)
-
 	render.NewTemplate(&app)
 
 	// scrapedData := utilities.ScrapeDataFromURL("https://www.2kratings.com/lists/top-100-highest-nba-2k-ratings")
 	// utilities.WriteToJson("player_data.json", scrapedData)
-	http.HandleFunc("/", handlers.Repo.Home)
-	http.HandleFunc("/player_ratings", handlers.Repo.Players)
 
 	fmt.Printf("Starting application on port%s\n", portNumber)
-	_ = http.ListenAndServe(portNumber, nil)
+
+	serve := &http.Server {
+		Addr: portNumber,
+		Handler: routes(&app),
+	}
+
+	err = serve.ListenAndServe()
+	log.Fatal(err)
 }
+
---
+++ cmd/web/routes.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"2K22/pkg/config"
+	"2K22/pkg/handlers"
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+)
+
+func routes(app *config.AppConfig) http.Handler {
+	mux := chi.NewRouter()
+
+	mux.Use(middleware.Recoverer)
+	mux.Use(NoSurf)
+
+	mux.Get("/", handlers.Repo.Home)
+	mux.Get("/player_ratings", handlers.Repo.Players)
+
+	return mux
+}
