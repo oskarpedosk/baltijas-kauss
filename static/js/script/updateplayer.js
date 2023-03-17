9bb49c56819cad694f5f5051074c889b486f4d76
oskarpedosk <oskar.pedosk@gmail.com>
2023-03-17 14:10:52 +0200

    ubuntu path

--- static/js/script/updateplayer.js
+++ static/js/script/updateplayer.js
@@ -10,7 +10,7 @@ puppeteer.use(StealthPlugin())
 
 const macPath = '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome'
 const ubuntuPath = '/usr/bin/chromium-browser'
-puppeteer.launch({executablePath: ubuntuPath, headless: true }).then(async browser => {
+puppeteer.launch({executablePath: '/usr/bin/chromium-browser', headless: true }).then(async browser => {
     page = await browser.newPage()
 
     const player_and_badges = await scrapePlayer(ratingsURL);
