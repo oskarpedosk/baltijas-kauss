6ebe16c4dac99823a865b7cfa77a05f5f69bba49
oskarpedosk <oskar.pedosk@gmail.com>
2023-04-13 14:13:10 +0300

    30 day session and in production = true

--- run.sh
+++ run.sh
@@ -2,4 +2,4 @@
 
 
 go build -o baltijaskauss ./cmd/web/*.go 
-./baltijaskauss -dbname=baltijas_kauss -dbuser=op -cache=false -production=false
+./baltijaskauss -dbname=baltijas_kauss -dbuser=op -cache=false -production=true
