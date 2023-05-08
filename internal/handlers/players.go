782350a77d0fcba9db8f4e14ee9338b1936c34f5
oskarpedosk <oskar.pedosk@gmail.com>
2023-05-08 23:40:18 +0300

    fixed some logging

--- internal/handlers/players.go
+++ internal/handlers/players.go
@@ -300,7 +300,6 @@ func (m *Repository) PostUpdatePlayer(w http.ResponseWriter, r *http.Request) {
 
 			// Parse the output as an array of two objects
 			var data []json.RawMessage
-			log.Println(data)
 			err = json.Unmarshal(output, &data)
 			if err != nil {
 				log.Println(err)
